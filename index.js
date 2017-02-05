34c8655517d8f536003913d7969e1d7a8ae69014
Jeroen De Swaef <jeroen@flavr.be>
2017-02-05 16:32:30 +0100

    Implemented bunch of other fuse apis

--- index.js
+++ index.js
@@ -36,14 +36,69 @@ function getFileList(entry) {
 
 function getAttr(entry, cb) {
   fs.stat(path.join(sourceDir, entry), function(err, stats) {
-    cb(stats);
+    let errCode = 0;
+    if (err) { 
+      errCode = err.errno;
+    }
+    cb(errCode, stats);
   });
 }
 
-function open(entry, cb) {
-   fs.open(path.join(sourceDir, entry), "r", function(error, fd) {
-     cb(fd);
+function open(entry, flags, cb) {
+   fs.open(path.join(sourceDir, entry), flags, function(err, fd) {
+    let errCode = 0;
+    if (err) { 
+      errCode = err.errno;
+    }
+    cb(errCode, fd);
    });
+}
+
+function create(entry, flags, cb) {
+  fs.open(path.join(sourceDir, entry), 'w+', function(err, fd) {
+    cb(fd);
+  });
+}
+
+function truncate(entry, len, cb) {
+  fs.truncate(path.join(sourceDir, entry), len, function(err) {
+    if (err) console.error('truncate', err);
+    cb();
+  });
+}
+
+function mkdir(entry, mode, cb) {
+  fs.mkdir(path.join(sourceDir, entry), mode, function(err) {
+    let errCode = 0;
+    if (err) { 
+      errCode = err.errno;
+    }
+    cb(errCode);
+  })
+}
+
+function chmod(entry, mode, cb) {
+  fs.chmod(path.join(sourceDir, entry), mode, function(err) {
+    if (err) console.error('chmod', err);
+    cb();
+  })
+}
+
+function rmdir(entry, cb) {
+  fs.rmdir(path.join(sourceDir, entry), function(err) {
+    let errCode = 0;
+    if (err) { 
+      errCode = err.errno;
+    }
+    cb(errCode);
+  });
+}
+
+function unlink(entry, cb) {
+  fs.unlink(path.join(sourceDir, entry), function(err) {
+    if (err) console.error('unlink', err);
+    cb();
+  });
 }
 
 // If previous run didn't clean up properly, do it now;
@@ -55,7 +110,11 @@ fuse.unmount(targetDir, function () {
     },
     getattr: function (entry, cb) {
       if (debug) console.log('getattr(%s)', entry);
-      getAttr(entry, function(stats) {
+      getAttr(entry, function(errCode, stats) {
+        if (errCode) {
+          cb(errCode);
+          return;
+        }
         if (stats) {
           cb(0, {
             mtime: stats.mtime,
@@ -72,14 +131,34 @@ fuse.unmount(targetDir, function () {
       });
     },
     open: function (entry, flags, cb) {
-      if (debug) console.log('open(%s, %d)', entry, flags)
-      open(entry, function(fd) {
-        cb(0, fd);
+      if (debug) console.log('open(%s, %d)', entry, flags);
+      open(entry, flags, function(errCode, fd) {
+        cb(errCode, fd);
       });
     },
     read: function (entry, fd, buf, len, pos, cb) {
       if (debug) console.log('read(%s, %d, %d, %d)', entry, fd, len, pos)
-      fs.read(fd, buf, 0, len, pos, function(error, bytesRead, buffer) {
+      fs.read(fd, buf, 0, len, pos, function(err, bytesRead, buffer) {
+        if (err) console.error('read', err);
+        return cb(buffer.length);
+      });
+    },
+    create: function (entry, flags, cb) {
+      if (debug) console.log('create(%s, %d)', entry, flags);
+      create(entry, flags, function(fd) {
+        cb(0, fd);
+      });
+    },
+    truncate: function (entry, size, cb) {
+      if (debug) console.log('truncate(%s, %d)', entry, size);
+      truncate(entry, function() {
+        cb(0);
+      });
+    },
+    write: function (entry, fd, buf, len, pos, cb) {
+      if (debug) console.log('write(%s, %d, %d, %d)', entry, fd, len, pos);
+      fs.write(fd, buf, 0, len, pos, function(err, bytesWritten, buffer) {
+        if (err) console.error('write', err);
         return cb(buffer.length);
       });
     },
@@ -87,7 +166,31 @@ fuse.unmount(targetDir, function () {
       fs.close(fd, function() {
         cb(0);
       });
-    }
+    },
+    mkdir: function(entry, mode, cb) {
+      if (debug) console.log('mkdir(%s, %d)', entry, mode);
+      mkdir(entry, mode, function() {
+        cb(0);
+      })
+    },
+    rmdir: function(entry, cb) {
+      if (debug) console.log('rmdir(%s)', entry);
+      rmdir(entry, function(errCode) {
+        cb(errCode);
+      });
+    },
+    unlink: function(entry, cb) {
+      if (debug) console.log('unlink(%s)', entry);
+      unlink(entry, function() {
+        cb(0);
+      })
+    },
+    chmod: function(entry, mode, cb) {
+      if (debug) console.log('chmod(%s, %d)', entry, mode);
+      chmod(entry, mode, function() {
+        cb(0);
+      })
+    },
   });
 
   process.on('SIGINT', function () {
